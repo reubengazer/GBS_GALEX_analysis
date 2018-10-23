9ab0218dbf327d11351a912e742dbfa089758040
Reuben Gazer <gazer@ualberta.ca>
2018-10-23 16:17:35 -0600

    Initial cleaning script for Galex UV data.

---
+++ CLEAN_chandra_data.py
@@ -0,0 +1,55 @@
+# Clean the Chandra X-ray data and export it as a dataset that's nicer to work with.
+# The Chandra data is located in a file called chandra_GBS_RAW.fits
+# It will be output currently as 'chandra_GBS_CLEAN.csv'.
+
+from astropy.io import fits
+import pandas as pd
+import numpy as np
+
+def fits_to_dataframe(fp):
+    """Clean Chandra X-Ray data, where the filepath (fp) is to a .fits file."""
+    with fits.open(fp) as data:
+        df = pd.DataFrame(data[1].data)
+    print('Read in original Chandra .fits file.')
+    return(df)
+
+def rename_cols(df):
+    """Rename the columns to more reasonable names.
+    Add 'CX' to each GBS_NAME numeric, so that GBS_NAME reads like 'CX210' or 'CX1092'."""
+    df = df.copy()
+    change_dict = {'CX':'GBS_NAME','Total':'X_COUNTS','RAJ2000':'X_RAJ2000','DEJ2000':'X_DEJ2000','Dpos':'X_ERR_R'}
+    df.rename(columns=change_dict,inplace=True)
+    df['GBS_NAME'] = ['CX'+str(num) for num in df['GBS_NAME']]
+    print('Renamed columns.')
+    return(df)
+
+def remove_cols(df):
+    """Remove unnecessary columns, as shown in the list cols_2_remove. 
+    Change these manually if need be."""
+    df = df.copy()
+    cols_2_remove = ['_RAJ2000','_DEJ2000']
+    df.drop(cols_2_remove,axis=1,inplace=True)
+    print('Removed unnecessary columns.')
+    return(df)
+
+def modify_x_error(df):
+    """Compute the 2-sigma positional error on Chandra, using Jonker et al. 2011.
+    We simply modify the current value in quadrature."""
+    df = df.copy()
+    df['X_ERR_R'] = [np.sqrt((P)**2.0 + (0.7)**2.0 + ((0.4085)**(-1)*0.16)**2.0) for P in df['X_ERR_R']]
+    print('Computed/modified X_ERR_R using Jonker et al. 2011 correction (2-sigma error).')
+    return(df)
+
+# Filepath of Chandra X-ray Data.
+fp = 'dat/chandra_GBS_RAW.fits'
+
+# Read in the .fits file through our function which converts it to a pandas DataFrame.
+df = fits_to_dataframe(fp)
+df_clean = df.pipe(rename_cols).pipe(remove_cols).pipe(modify_x_error)
+print('The header of the cleaned dataframe:')
+print(df_clean.head())
+
+# Output filepath of cleaned data.
+fp_out = 'dat/chandra_GBS_CLEAN.csv'
+df_clean.to_csv(fp_out)
+print('Saved cleaned data here: {}'.format(fp_out))
---
+++ CLEAN_galex_data.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import pylab as plt
+import seaborn as sns
+
+# Right and left half of Galex data.
+# We queired CasJobs which holds the Galex NUV data.
+# Due to the way the longitude in degrees works, we needed to query 
+# all Galex points in the GBS region, which is continuous section
+# with longitudes between ~ 357-360 and 0-3 degrees.
+# Due to this split over 360, we need two queries for right and left halves, then we stack.
+
+def stack_halves(right,left):
+    glx = pd.concat([right,left],axis=1)
+    print('Imported Galex halves and stacked.')
+    return(glx)
+
+def convert_glon(df):
+    """Convert longitude values < 360 to negative equivalents where 360 meets 0."""
+    df = df.copy()
+    df.apply(lambda long: long-360 if long =< 360)
+    print('Converted longitudes < 360 to negative values.')
+    return(df)
+
+def trim(df):
+    """Trim the Galex observations by removing all points out of the standard GBS region:
+    We need to cut out the center horizontal chunk not included in the GBS between ~ -1 < lat < +1."""
+    df = df.copy()
+    len_init = len(df)
+    long_max,long_min = 3.1, -3.1
+    df = df[(df['glon'] < long_max) & (df['glon'] > -long_min)] # cut longitude.
+    df = df[(df['glat'] > 0.83) % (df['glat'] < -0.83)] # cut latitude by removing middle.
+    len_fin = len(df)
+    print('Trimmed Galex observations to represent the GBS area, reducing observations from {len_init} to {len_fin} lines.'.format(**locals()))
+    return(df)
+
+def rename_cols(df):
+    """Rename the columns to more reasonable names."""
+    df = df.copy()
+    change_dict = {'nuv_mag':'NUV_MAG','nuv_magerr':'NUV_MAG_ERR','nuv_flux':'NUV_FLUX',\
+    'nuv_fluxerr':'NUV_FLUX_ERR','ra':'GLX_RA','dec':'GLX_DEC','fov_radius':'DIST_2_FOV'}
+    df.rename(columns=change_dict,inplace=True)
+    print('Renamed columns.')
+    return(df)
+
+def remove_duplicates(df):
+    """Remove the duplicate observations in the Galex NUV dataset from CasJobs.
+    There are many observations that in-fact represent the same light-source, 
+    but are in different images/surveys. Here we continually add sources to a final list, 
+    each time checking that there does not exist a source "close" to it already, since
+    the NUV density is much lower than 1 per 10" radius around any point."""
+    df = df.copy()
+    # Sort df by DIST_2_FOV as they have the smallest observational errors among duplicates.
+    df.sort('DIST_2_FOV')
+    # Create empty dataframe for our unduplicated observations.
+    real_sources = pd.DataFrame()
+    # Initialize it with the first source.
+    real_sources.append(df.iloc[0])
+
+    def is_duplicate(point,df,tol=None):
+        """Return a boolean TRUE or FALSE if there exists an observation in df
+        that is sufficiently close to 'point'.
+        point: an observation from the Galex dataset.
+        df: a comparison set.
+        tol: distance tolerance to accept or reject a source."""
+        df = df.copy()
+        # Set tolerance if not None.
+        if tol == None: 
+            tol = 0.000694444 # degrees, or 2.5 arcseconds.
+        # Distance between point and all other observations in df, Euclidean 2-norm.
+        distance = np.sqrt((point['GLX_RA']-df['GLX_RA'])**2.0 + (point['GLX_DEC']-df['GLX_DEC'])**2.0)
+        # If any are close, return True.
+        if any([dist < tol for dist in distance]):
+            return(True)
+        else:
+            return(False)
+
+    # Loop through data, only select those that have no duplicates within tol.
+    for i, point in df.drop(0,axis=0).iterrows(): # don't do the 0th observation as this is the initial one!
+        if is_duplicate(point,real_sources) != True: # if it's unique in the set thus far, no dupe
+            real_sources.append(point)
+
+    print('Removed duplicate Galex observations, reducing observations from {} to {} lines.'.format(len(df),len(real_sources)))
+    return(real_sources)
+
+# Filepaths of right and left halves of the Galex data in the rough GBS region.
+glx_right = pd.read_csv('dat/galex_right_half_RAW.csv')
+glx_left = pd.read_csv('dat/galex_left_half_RAW.csv')
+df = stack_halves(glx_right,glx_left)
+df.to_csv('dat/galex_RAW.csv')
+
+# Apply each of the cleaning functions to the dataframe and output the header.
+df_clean = df.pipe(convert_glon).pipe(trim).pipe(rename_cols).pipe(remove_duplicates)
+print('The header of the cleaned dataframe:')
+print(df_clean.head())
+
+# Output filepath of cleaned data.
+fp_out = 'dat/galex_CLEAN.csv'
+df_clean.to_csv(fp_out)
+print('Saved cleaned data here: {}'.format(fp_out))
